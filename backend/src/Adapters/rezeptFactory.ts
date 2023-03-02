8cb7804dae6327f05808282b54e6b37d4f96fb3b
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-03-02 16:07:09 +0100

    ✨ add updateRezept function

--- backend/src/Adapters/rezeptFactory.ts
+++ backend/src/Adapters/rezeptFactory.ts
@@ -66,4 +66,47 @@ export class RezeptFactory {
 
     return rezept
   }
+
+  async updateRezept(rezept: Rezept): Promise<Rezept> {
+    await dataSource.transaction(async (transactionalEntityManager) => {
+      // get repositories
+      const rezeptRepository = transactionalEntityManager.getRepository(RezeptEntity)
+      const zutatRepository = transactionalEntityManager.getRepository(ZutatEntity)
+      const rezeptZutatRepository = transactionalEntityManager.getRepository(RezeptZutatEntity)
+
+      //update Rezept
+      const dbRezept = await rezeptRepository.findOneByOrFail({id: rezept.getId()})
+      if (dbRezept && dbRezept.id === rezept.getId()) {
+        const updatedRezept: RezeptEntity = {id: rezept.getId(), name: rezept.name, aufwand: rezept.aufwand}
+        rezeptRepository.save(updatedRezept)
+      }
+
+      //update Zutaten and RezeptZutaten
+      const rezeptZutaten = rezept.rezeptZutaten
+      await Promise.all(rezeptZutaten.map(async rezeptZutat => {
+        const zutat = rezeptZutat.zutat
+        const dbZutat = await zutatRepository.findOneBy({name: zutat.name, typ: zutat.typ})
+
+        // create missing Zutat
+        if (!dbZutat) {
+          const erstellteZutat = await zutatRepository.save(zutat)
+          zutat.setId(erstellteZutat.id)
+        }
+
+        const neueRezeptZutat: RezeptZutatEntity = {
+          rezeptId: rezept.getId(),
+          zutatId: zutat.getId(),
+          mengeWert: rezeptZutat.menge.wert,
+          mengeEinheit: rezeptZutat.menge.einheit,
+          mengeTyp: rezeptZutat.menge.typ
+        }
+        
+        const dbRezeptZutat = await rezeptZutatRepository.save(neueRezeptZutat)
+        rezeptZutat.zutat.setId(dbRezeptZutat.zutatId)
+        rezeptZutat.setRezeptId(dbRezeptZutat.rezeptId)
+      }))
+    })
+
+    return rezept
+  }
 }
