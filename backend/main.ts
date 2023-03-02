5660ec45761789801cfcc600ca5ab7c0cb3c0356
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-03-02 16:07:26 +0100

    ✨ add post endpoint for rezept

--- backend/main.ts
+++ backend/main.ts
@@ -2,7 +2,12 @@ import {Request, Response} from 'express'
 import { Get, Post } from './src/decorators'
 import { Server } from './src/server'
 import { dataSource } from './src/Adapters/datenbankAdapter'
+import { RezeptFactory } from './src/Adapters/rezeptFactory'
+import { Menge, Rezept, RezeptBodyJSON, RezeptZutat, Zutat } from 'kern-util'
+import { arraysEqual } from './src/helpers'
 
+
+const rezeptFactory = RezeptFactory.getInstance()
 
 const server = new Server()
 
@@ -34,6 +39,23 @@ class Routes {
     @Get('/errTest')
     getErrorTest() {
         throw new Error('Testing')
+    }
+
+    @Post('/rezept')
+    async postRezept(req: Request<{}, {}, RezeptBodyJSON>, res: Response) {
+        const expectedKeys: string[] = [ 'id', 'name', 'aufwand', 'rezeptZutaten' ]
+        if (!arraysEqual(Object.keys(req.body), expectedKeys)) throw new Error('body type error')
+
+        const rezeptData: RezeptBodyJSON = req.body
+        const rezeptZutaten: RezeptZutat[] = []
+        rezeptData.rezeptZutaten.forEach(rezeptZutat => {
+            const zutat: Zutat = new Zutat(rezeptZutat.zutat.id, rezeptZutat.zutat.name, rezeptZutat.zutat.typ)
+            const menge: Menge = new Menge(rezeptZutat.menge.wert, rezeptZutat.menge.einheit)
+            rezeptZutaten.push(new RezeptZutat(rezeptZutat.rezeptId, zutat, menge))
+        })
+        const rezept = new Rezept(rezeptData.id, rezeptData.name, rezeptData.aufwand, rezeptZutaten)
+
+        return await rezeptFactory.createRezept(rezept)
     }
 }
 
