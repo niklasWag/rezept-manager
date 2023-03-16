191c701e65cdb4539ab7a32358dbc56703c60709
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-03-16 14:20:30 +0100

    🐛 fix mengenNormalisieren after test

--- kern-util/src/abstraktion/Umrechnungen.ts
+++ kern-util/src/abstraktion/Umrechnungen.ts
@@ -1,6 +1,6 @@
 import { MengenEinheit } from "./MengenEinheiten";
 
 export function mengenNormalisieren(wert: number, einheit: MengenEinheit): number {
-  if (einheit === (MengenEinheit.g || MengenEinheit.ml || MengenEinheit.Stück)) return wert
+  if (einheit === MengenEinheit.g || einheit === MengenEinheit.ml || einheit === MengenEinheit.Stück) return wert
   return wert * 1000
 }
