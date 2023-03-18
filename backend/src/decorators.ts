2aa9e28283ab0116a17f8643e296c8474611359a
Maier <timo.maier02@sap.com>
2023-03-18 17:46:06 +0100

    Added put and delete decorator

--- backend/src/decorators.ts
+++ backend/src/decorators.ts
@@ -59,3 +59,61 @@ export function Post (path: string): MethodDecorator {
     server.app.post(path, response);
   }
 }
+
+export function Put (path: string): MethodDecorator {
+  const server = getServer()
+  return function (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const response = async (req: Request, res: Response) => {
+      try {
+        const original = await descriptor.value(req, res);
+
+        res.status(200).send(original);
+      } catch (err: any) {
+        if (err.message === 'body type error') {
+          res.status(400).send('Body does not match required type')
+        } else {
+          res.status(500).json({
+            message: 'An error occurred',
+            error: err.message,
+            stack: err.stack
+          })
+        }
+      }
+    }
+
+    server.app.put(path, response);
+  }
+}
+
+export function Delete (path: string): MethodDecorator {
+  const server = getServer()
+  return function (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const response = async (req: Request, res: Response) => {
+      try {
+        const original = await descriptor.value(req, res);
+
+        res.status(200).send(original);
+      } catch (err: any) {
+        if (err.message === 'Entity not found error') {
+          res.status(404).send('Entity to be deleted was not found')
+        } else {
+          res.status(500).json({
+            message: 'An error occurred',
+            error: err.message,
+            stack: err.stack
+          })
+        }
+      }
+    }
+
+    server.app.delete(path, response);
+  }
+}
