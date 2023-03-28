f7656fc24e67d6f8d98b24bcae76fa28ac370926
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-03-28 11:17:56 +0200

    🔒️ update cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -31,7 +31,7 @@ export class Server {
     this.app.use(function (req: Request, res: Response, next: NextFunction) {
       res.setHeader("Access-Control-Allow-Origin", "*")
       res.setHeader("Access-Control-Allow-Credentials", "true")
-      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
       res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization")
       next()
   })
