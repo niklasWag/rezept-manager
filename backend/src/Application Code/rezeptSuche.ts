d3b89abb3f6792f732d9879325731d3a17c2c730
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-03-29 11:16:18 +0200

    🐛 fix pass by reference bug

--- backend/src/Application Code/rezeptSuche.ts
+++ backend/src/Application Code/rezeptSuche.ts
@@ -1,24 +1,24 @@
-import { Rezept, Zutat } from "kern-util";
+import { Rezept } from "kern-util";
 
 export function rezeptSuchen(rezepte: Rezept[], rezeptFilter: RezeptFilter): Rezept[] {
+  const lokaleRezepte = [...rezepte]
   if (rezeptFilter.aufwand.length === 0 && rezeptFilter.zutatenIds.length === 0) return rezepte
   let filteredRezepte: Rezept[] = []
   if (rezeptFilter.zutatenIds.length > 0) {
     rezeptFilter.zutatenIds.forEach(zutatId => {
-      rezepte.forEach(rezept => {
+      lokaleRezepte.forEach(rezept => {
         rezept.rezeptZutaten.forEach(rezeptZutat => {
           if (rezeptZutat.getZutatId() === zutatId) {
             filteredRezepte.push(rezept)
-            rezepte.splice(rezepte.indexOf(rezept), 1)
+            lokaleRezepte.splice(lokaleRezepte.indexOf(rezept), 1)
           }
         })
       })
     })
   } else {
-    filteredRezepte = rezepte
+    filteredRezepte = lokaleRezepte
   }
   if (rezeptFilter.aufwand.length > 0) {
-    console.log(filteredRezepte)
     return filteredRezepte.filter(rezept => rezeptFilter.aufwand.includes(rezept.aufwand.toString()))
   }
   return filteredRezepte
