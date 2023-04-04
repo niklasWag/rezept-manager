cceb8291e3e72f35d164d220734b0e1a9f049e12
Niklas Wagner <77329334+niklasWag@users.noreply.github.com>
2023-04-04 16:07:30 +0200

    🐛 fix rezept search array splice

--- backend/src/Application Code/rezeptSuche.ts
+++ backend/src/Application Code/rezeptSuche.ts
@@ -2,15 +2,16 @@ import { Rezept } from "kern-util";
 
 export function rezeptSuchen(rezepte: Rezept[], rezeptFilter: RezeptFilter): Rezept[] {
   const lokaleRezepte = [...rezepte]
+  const usedRezeptIds: number[] = []
   if (rezeptFilter.aufwand.length === 0 && rezeptFilter.zutatenIds.length === 0) return rezepte
   let filteredRezepte: Rezept[] = []
   if (rezeptFilter.zutatenIds.length > 0) {
     rezeptFilter.zutatenIds.forEach(zutatId => {
       lokaleRezepte.forEach(rezept => {
         rezept.rezeptZutaten.forEach(rezeptZutat => {
-          if (rezeptZutat.getZutatId() === zutatId) {
+          if (rezeptZutat.getZutatId() === zutatId && !usedRezeptIds.includes(rezept.getId())) {
             filteredRezepte.push(rezept)
-            lokaleRezepte.splice(lokaleRezepte.indexOf(rezept), 1)
+            usedRezeptIds.push(rezept.getId())
           }
         })
       })
